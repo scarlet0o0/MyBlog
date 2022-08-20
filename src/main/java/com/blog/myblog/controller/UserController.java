aef89523e9d80a67f2c3a35cb5c06d3d24ce9235
scarlet0o0 <ghd7673@naver.com>
2022-08-20 23:58:59 +0900

    UserController 추가

--- src/main/java/com/blog/myblog/controller/UserController.java
+++ src/main/java/com/blog/myblog/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.blog.myblog.controller;
 
+import com.blog.myblog.dto.SignupRequestDto;
+import com.blog.myblog.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     // 회원 로그인 페이지
     @GetMapping("/user/login")
@@ -16,5 +24,12 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signup() {
         return "signup";
+    }
+
+    // 회원 가입 요청 처리
+    @PostMapping("/user/signup")
+    public String registerUser(SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+        return "redirect:/user/login";
     }
 }
