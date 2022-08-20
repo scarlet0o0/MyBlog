41b95120bba8833a421ccd2d0e18f336152ff44f
scarlet0o0 <ghd7673@naver.com>
2022-08-21 00:47:41 +0900

    커밋 커밋

--- src/main/java/com/blog/myblog/security/WebSecurityConfig.java
+++ src/main/java/com/blog/myblog/security/WebSecurityConfig.java
@@ -10,8 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
+        http.csrf()
+                .ignoringAntMatchers("/user/**");
 
         http.authorizeRequests()
+                // 회원 관리 처리 API 전부를 login 없이 허용
+                .antMatchers("/user/**").permitAll()
                 // 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
