42922fd959b3e966bbd2540cdf6cb02bcde82729
scarlet0o0 <ghd7673@naver.com>
2022-08-22 19:22:55 +0900

    유저 로그인 기능 추가

--- src/main/java/com/blog/myblog/controller/UserController.java
+++ src/main/java/com/blog/myblog/controller/UserController.java
@@ -1,35 +1,42 @@
 package com.blog.myblog.controller;
 
+import com.blog.myblog.domain.SingleResponse;
+import com.blog.myblog.domain.Users;
 import com.blog.myblog.dto.SignupRequestDto;
+import com.blog.myblog.dto.SignupResponseDto;
+import com.blog.myblog.service.ResponseService;
 import com.blog.myblog.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
     private final UserService userService;
+    private final ResponseService responseService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ResponseService responseService) {
         this.userService = userService;
+        this.responseService = responseService;
     }
 
     // 회원 로그인 페이지
-    @GetMapping("/user/login")
-    public String login() {
-        return "login";
-    }
-
-    // 회원 가입 페이지
-    @GetMapping("/user/signup")
-    public String signup() {
-        return "signup";
-    }
+//    @GetMapping("/user/login")
+//    public String login() {
+//        return "login";
+//    }
+//
+//    // 회원 가입 페이지
+//    @GetMapping("/user/signup")
+//    public String signup() {
+//        return "signup";
+//    }
 
     // 회원 가입 요청 처리
     @PostMapping("/user/signup")
-    public String registerUser(SignupRequestDto requestDto) {
-        userService.registerUser(requestDto);
-        return "redirect:/user/login";
+    public SingleResponse<SignupResponseDto> registerUser(@RequestBody SignupRequestDto requestDto) {
+        Users users = userService.registerUser(requestDto);
+        SignupResponseDto signupResponseDto = new SignupResponseDto(users);
+        return responseService.getSingleResponse(signupResponseDto);
     }
 }
--- src/main/java/com/blog/myblog/service/UserService.java
+++ src/main/java/com/blog/myblog/service/UserService.java
@@ -4,32 +4,37 @@ import com.blog.myblog.domain.Users;
 import com.blog.myblog.dto.SignupRequestDto;
 import com.blog.myblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService{
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-    private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public void registerUser(SignupRequestDto requestDto) {
+    public Users registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
+
         // 회원 ID 중복 확인
-        System.out.println(username);
-        System.out.println(password);
         Optional<Users> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
 
+        // 패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
         Users users = new Users(username, password);
         userRepository.save(users);
+
+        return users;
     }
 }
