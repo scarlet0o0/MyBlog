1fe92c275201940a08bb417461ef3e32505fc5e0
scarlet0o0 <ghd7673@naver.com>
2022-08-23 05:02:02 +0900

    WebSecurityConfig 변경

--- src/main/java/com/blog/myblog/security/WebSecurityConfig.java
+++ src/main/java/com/blog/myblog/security/WebSecurityConfig.java
@@ -1,16 +1,37 @@
 package com.blog.myblog.security;
 
+import com.blog.myblog.jwt.JwtAccessDeniedHandler;
+import com.blog.myblog.jwt.JwtAuthenticationEntryPoint;
+import com.blog.myblog.jwt.JwtSecurityConfig;
+import com.blog.myblog.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public WebSecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 
     @Bean
     public BCryptPasswordEncoder encodePassword() {
@@ -27,28 +48,65 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
 
-        http.authorizeRequests()
-                // 그 외 어떤 요청이든 '인증'
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
                 .anyRequest().authenticated()
+
                 .and()
-                // [로그인 기능]
-                .formLogin()
-                // 로그인 처리 (POST /user/login)
-                .loginProcessingUrl("/user/login")
-                // 로그인 처리 후 성공 시 URL
-                .defaultSuccessUrl("/")
-                // 로그인 처리 후 실패 시 URL
-                .failureUrl("/user/login?error")
-                .permitAll()
-                .and()
-                // [로그아웃 기능]
-                .logout()
-                // 로그아웃 처리 URL
-                .logoutUrl("/user/logout")
-                .permitAll();
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+
+//        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
+//        http.csrf()
+//                .ignoringAntMatchers("/user/**")
+//                .ignoringAntMatchers("/api/memo/**");
+//
+//        http.authorizeRequests()
+//                // 회원 관리 처리 API 전부를 login 없이 허용
+//                .antMatchers("/user/**").permitAll()
+//                // 메모 보기 관련 api 들은 로그인 없이 허용
+//                .antMatchers("/api/memo/**").permitAll()
+//
+//                // 그외 어떤 요청이든 '인증'
+//                .anyRequest().authenticated()
+//                .and()
+//                // [로그인기능]
+//                .formLogin()
+//                // 로그인 처리 (POST /user/login)
+//                .loginProcessingUrl("/user/login")
+//                // 로그인 처리 후 성공 시 URL
+//                .defaultSuccessUrl("/")
+//                // 로그인 처리 후 실패 시 URL
+//                .failureUrl("/user/login?error")
+//                .permitAll()
+//                .and()
+//                // [로그아웃 기능]
+//                .logout()
+//                //로그아웃 처리 URL
+//                .logoutUrl("/user/logout")
+//                .permitAll();
+
     }
 }
